80ec6bb28ff44da33ac4a6537b352e2d58b306c0
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-10 17:51:24 +0200

    added 2 methods into the interface TransportCreation

--- src/TransportCreation.java
+++ src/TransportCreation.java
@@ -1,4 +1,6 @@
+import java.util.List;
 /**
+ * 
  * An interface for creating transports.
  */
 public interface TransportCreation extends TransportManagement {
@@ -10,4 +12,21 @@ public interface TransportCreation extends TransportManagement {
      * @param capacity the capacity of the transport
      */
     void createTransport(String type, String route, int capacity);
+
+    /**
+     * Adds a predefined line between zones.
+     *
+     * @param originZone      the origin zone of the line
+     * @param destinationZone the destination zone of the line
+     * @param transports      the list of transports for the predefined line
+     */
+    void addPredefinedLine(Zone originZone, Zone destinationZone, List<Transport> transports);
+     /**
+     * Calculates the congestion of a predefined line between zones.
+     *
+     * @param originZone      the origin zone of the line
+     * @param destinationZone the destination zone of the line
+     * @return the congestion of the line
+     */
+    int calculateCongestion(Zone originZone, Zone destinationZone);
 }
