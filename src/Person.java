108e639444f412e547d589876c8b983ac4f34e1c
Gabriele Rubboli Petroselli <gabriele.rubboli@studio.unibo.it>
2024-04-10 11:42:51 +0200

    created interface Person

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,19 @@
-public interface Person {
+
+/**
+ * An interface for modelling a person
+ * 
+ * @param S, the state of the person
+ */
+public interface Person<S> {
+
+    /**
+	 * @return the state of the person
+	 */
+    <S> getState();
+    
+    /**
+	 * @return the money holded by the person
+	 */
+    int getMoney();
     
 }
