10f029f2aff4eb1baf421bbb0dec8312a6388011
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-10 12:19:56 +0200

    added interface for graphics of the transport

---
+++ src/TransportGraphics.java
@@ -0,0 +1,15 @@
+/**
+ * An interface for managing the graphical representation of transports.
+ */
+public interface TransportGraphics extends TransportManagement {
+    /**
+     * Displays transports on the map.
+     */
+    void displayTransportsOnMap();
+
+    /**
+     * Updates the graphical representation of transports on the map.
+     */
+    void updateTransportGraphics();
+}
+
