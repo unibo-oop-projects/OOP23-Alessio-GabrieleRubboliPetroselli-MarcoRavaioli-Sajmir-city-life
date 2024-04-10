56a7a6514b0b20f1557a15e18380584bd995537b
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-10 11:51:19 +0200

    adding a Transport interface base

---
+++ src/TransportManagement.java
@@ -0,0 +1,18 @@
+/**
+ * An interface for modelling a person
+ * 
+ * 
+ */
+public interface TransportManagement {
+    /**
+	 * @return the congestion of the transportLine
+	 */
+    int getCongestion();
+    /**
+	 * @return the capacity of the transportLine
+	 */
+    int getCapacity();
+
+
+
+}
