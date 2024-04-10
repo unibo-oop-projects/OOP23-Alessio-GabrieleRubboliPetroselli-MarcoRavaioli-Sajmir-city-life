7f0ae2b013c8ca1e5bc4eddc0dddb50abff60f99
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-10 12:12:14 +0200

    added interface for the state of transport

---
+++ src/TransportState.java
@@ -0,0 +1,9 @@
+/**
+ * An interface for monitoring the state of transports.
+ */
+public interface TransportState extends TransportManagement {
+    /**
+     * Monitors the state of transports.
+     */
+    void monitorTransportState();
+}
