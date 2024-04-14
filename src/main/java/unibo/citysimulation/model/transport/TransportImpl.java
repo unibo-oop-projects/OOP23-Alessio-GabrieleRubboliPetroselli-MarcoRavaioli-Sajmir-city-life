e9953f228ceca0bdfddac48147ba2e171e10bb56
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-14 17:12:25 +0200

    implementation of Transport with some additional methods

--- src/main/java/unibo/citysimulation/model/transport/TransportImpl.java
+++ src/main/java/unibo/citysimulation/model/transport/TransportImpl.java
@@ -1,23 +1,36 @@
 package unibo.citysimulation.model.transport;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TransportImpl implements Transport {
-
-    private int capacity;
     private int congestion;
+    private int capacity;
+    public Map<Zone, Map<Zone, List<Transport>>> predefinedLines;
 
-    public TransportImpl(int capacity,int congestion){
-        this.congestion=congestion;
-        this.capacity=capacity;
+    public TransportImpl(int congestion, int capacity) {
+        this.congestion = congestion;
+        this.capacity = capacity;
+        this.predefinedLines = new HashMap<>();
     }
 
     @Override
     public int getCongestion() {
-        return this.congestion;
+        return congestion;
     }
 
     @Override
     public int getCapacity() {
-        return this.capacity;
+        return capacity;
     }
-    
+
+    public void addPredefinedLine(Zone originZone, Zone destinationZone) {
+        List<Transport> transports = new ArrayList<>();
+        transports.add(this);
+
+        predefinedLines.computeIfAbsent(originZone, k -> new HashMap<>())
+                .put(destinationZone, transports);
+    }
 }
