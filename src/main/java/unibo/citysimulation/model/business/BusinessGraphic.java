45eeefab22294babeb8dbd287e0a88d6c721eaaa
Alessio <alessiobifulco24@gmail.com>
2024-04-18 15:42:13 +0200

    created a based interface for business graphic

---
+++ src/main/java/unibo/citysimulation/model/business/BusinessGraphic.java
@@ -0,0 +1,10 @@
+package unibo.citysimulation.model.business;
+
+/**
+ * Interface representing graphical aspects of a business entity.
+ */
+public interface BusinessGraphic {
+    // This interface is currently empty. Additional methods related to graphical
+    // representation of the business can be added in the future.
+}
+
