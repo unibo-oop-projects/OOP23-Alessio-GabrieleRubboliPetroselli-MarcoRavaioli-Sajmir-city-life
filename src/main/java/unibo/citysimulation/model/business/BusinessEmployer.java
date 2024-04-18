36af1db2e264626753b2de3cb2de8457cb362d22
Alessio <alessiobifulco24@gmail.com>
2024-04-18 15:45:40 +0200

    created based interface for business employer

---
+++ src/main/java/unibo/citysimulation/model/business/BusinessEmployer.java
@@ -0,0 +1,10 @@
+package unibo.citysimulation.model.business;
+
+/**
+ * Interface representing employee-related aspects of a business entity.
+ */
+public interface BusinessEmployer {
+    // This interface is currently empty. Additional methods related to managing
+    // employees within the business can be added in the future.
+}
+
