ccb253f442bd763c63c7ab7d3469672d87fdfcf9
Alessio <alessiobifulco24@gmail.com>
2024-04-18 15:44:08 +0200

    created a based interface for business financial

---
+++ src/main/java/unibo/citysimulation/model/business/BusinessFinancial.java
@@ -0,0 +1,10 @@
+package unibo.citysimulation.model.business;
+
+/**
+ * Interface representing financial aspects of a business entity.
+ */
+public interface BusinessFinancial {
+    // This interface is currently empty. Additional methods related to financial
+    // operations of the business can be added in the future.
+}
+
