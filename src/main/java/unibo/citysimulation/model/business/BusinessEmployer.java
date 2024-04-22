8ed7dead99a040922a0291a4ecaf4dbfb2deb183
Alessio <alessiobifulco24@gmail.com>
2024-04-22 14:14:22 +0200

    added method to check, count, get a list and get by id in the interface businessemployer

--- src/main/java/unibo/citysimulation/model/business/BusinessEmployer.java
+++ src/main/java/unibo/citysimulation/model/business/BusinessEmployer.java
@@ -9,4 +9,17 @@ public interface BusinessEmployer {
 
     // Method to fire an employee
     void fire(Employee employee);
+
+    // Method to check if the business has employees
+    boolean hasEmployees();
+
+    // Method to count the number of employees
+    int countEmployees();
+
+    // Method to get the list of employees
+    List<Employee> getEmployees();
+
+    // Method to get an employee by ID
+    Employee getEmployeeById(int id);
+    
 }
