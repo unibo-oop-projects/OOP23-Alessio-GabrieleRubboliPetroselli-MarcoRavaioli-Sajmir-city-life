f49fba23c812dc4b0f4b2a68c02e4db9dbaf7278
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-23 15:58:14 +0200

    added method getCongestion()

--- src/main/java/unibo/citysimulation/model/transport/TransportLine.java
+++ src/main/java/unibo/citysimulation/model/transport/TransportLine.java
@@ -22,30 +22,17 @@ public class TransportLine {
     }
 
     // Getter and setter methods for other fields, if needed
-
-
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
     public int getStartHour() {
         return startHour;
     }
-
-    public void setStartHour(int startHour) {
-        this.startHour = startHour;
-    }
-
     public int getEndHour() {
         return endHour;
     }
 
-    public void setEndHour(int endHour) {
-        this.endHour = endHour;
+    public double getCongestion(){
+        return (double)(personInLine*100/capacity);
     }
-
 }
