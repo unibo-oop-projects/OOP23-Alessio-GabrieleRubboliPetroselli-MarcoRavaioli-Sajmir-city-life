124cd6ded53664ff8e85544d64aed8ae73ede406
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-24 18:24:21 +0200

    added methods  getPredefinedLines() and getPredefinedDurations()

--- src/main/java/unibo/citysimulation/model/transport/TransportLineImpl.java
+++ src/main/java/unibo/citysimulation/model/transport/TransportLineImpl.java
@@ -1,5 +1,6 @@
 package unibo.citysimulation.model.transport;
-
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represents a transport line within the city simulation.
@@ -11,8 +12,12 @@ public class TransportLineImpl {
     private int endHour; // Ending hour of service
     private int personInLine=0;
     private int durationLine; // Duration Line in minutes
+    private Map<Zone, Zone> predefinedLines;
+    private Map<Zone, Integer> predefinedDurations;
 
     public TransportLineImpl(String name, int startHour, int endHour,int capacity,int durationLine) {
+        this.predefinedLines = new HashMap<>();
+        this.predefinedDurations = new HashMap<>();
         this.name = name;
         this.startHour = startHour;
         this.endHour = endHour;
@@ -33,5 +38,16 @@ public class TransportLineImpl {
 
     public double getCongestion(){
         return (double)(personInLine*100/capacity);
+    }
+    public void addPredefinedLine(Zone originZone, Zone destinationZone, int durationLine) {
+        predefinedLines.put(originZone, destinationZone);
+        predefinedDurations.put(originZone, durationLine);
+    }
+    public Map<Zone, Zone> getPredefinedLines() {
+        return predefinedLines;
+    }
+
+    public Map<Zone, Integer> getPredefinedDurations() {
+        return predefinedDurations;
     }
 }
