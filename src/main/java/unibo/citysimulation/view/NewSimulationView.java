01aa6ecdb5234a7bbea798b01ffecbe35c8731c5
MarcoRavaioli <marco.ravaioli8@studio.unibo.it>
2024-04-26 12:42:53 +0200

    Now sidePanels have the correct width

--- src/main/java/unibo/citysimulation/view/NewSimulationView.java
+++ src/main/java/unibo/citysimulation/view/NewSimulationView.java
@@ -13,6 +13,9 @@ import java.awt.*;
 
 public class NewSimulationView extends JFrame {
     InfoPanel infoPanel = new InfoPanel(Color.GREEN);
+    ClockPanel clockPanel = new ClockPanel(Color.RED);
+    InputPanel inputPanel = new InputPanel(Color.BLUE);
+    GraphicsPanel graphicsPanel = new GraphicsPanel(Color.YELLOW);
 
     public NewSimulationView() {
         setTitle(ConstantAndResourceLoader.APPLICATION_NAME);
@@ -30,10 +33,6 @@ public class NewSimulationView extends JFrame {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setSize((int) (screenSize.getWidth() * ConstantAndResourceLoader.SCREEN_SIZE_PERCENTAGE),
                 (int) (screenSize.getHeight() * ConstantAndResourceLoader.SCREEN_SIZE_PERCENTAGE));
-        /*
-        Container pane = getContentPane();
-        pane.setLayout(null);
-        pane.setBackground(Color.LIGHT_GRAY);*/
     }
 
     private void createComponents() {
@@ -48,19 +47,36 @@ public class NewSimulationView extends JFrame {
     }
 
     private void createSidePanels() {
+
+        int sidePanelWidth = getSize().width / 4;
+
         // Creazione del pannello sinistro superiore con due sottopannelli di colore diverso
         JPanel leftPanel = new JPanel(new GridLayout(2, 1));
-        leftPanel.add(new InputPanel(Color.BLUE), BorderLayout.CENTER);
-        leftPanel.add(infoPanel, BorderLayout.SOUTH);
+        inputPanel.setSize(new Dimension(sidePanelWidth, 100));//getSize().height / 4 * 3));
+        leftPanel.add(inputPanel, BorderLayout.CENTER);
+        infoPanel.setPreferredSize(new Dimension(sidePanelWidth, getSize().height / 4));
+        //leftPanel.add(infoPanel, BorderLayout.SOUTH);
 
         // Creazione del pannello destro superiore con due sottopannelli di colore diverso
         JPanel rightPanel = new JPanel(new GridLayout(2, 1));
-        rightPanel.add(new ClockPanel(Color.PINK), BorderLayout.NORTH);
-        rightPanel.add(new GraphicsPanel(Color.RED), BorderLayout.CENTER);
+        clockPanel.setPreferredSize(new Dimension(sidePanelWidth, getHeight() / 8));
+        rightPanel.add(clockPanel, BorderLayout.NORTH);
+        graphicsPanel.setPreferredSize(new Dimension(sidePanelWidth, getHeight() / 8 * 7));
+        //rightPanel.add(graphicsPanel, BorderLayout.CENTER);
 
         // Aggiunta dei pannelli laterali al frame
         add(leftPanel, BorderLayout.WEST);
         add(rightPanel, BorderLayout.EAST);
+
+        revalidate();
+        repaint();
+
+        System.out.println("Side panel width: " + sidePanelWidth);
+        System.out.println("Input panel height: " + inputPanel.getPreferredSize().height);
+        System.out.println("Info panel height: " + infoPanel.getPreferredSize().height);
+        System.out.println("Clock panel height: " + clockPanel.getPreferredSize().height);
+        System.out.println("Graphics panel height: " + graphicsPanel.getPreferredSize().height);
+
     }
 
 }
