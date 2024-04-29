a53598958dbf1bf4592c4e003b6fe9d079d3f36e
Sajmir <sajmir.buzi@studio.unibo.it>
2024-04-29 12:53:03 +0200

    fixed constructor

--- src/main/java/unibo/citysimulation/model/transport/Zone.java
+++ src/main/java/unibo/citysimulation/model/transport/Zone.java
@@ -5,26 +5,17 @@ package unibo.citysimulation.model.transport;
  */
 public class Zone {
     private String name;
-    private TransportLine transportLine;
 
-    public Zone(String name, TransportLine transportLine) {
+    public Zone(String name) {
         this.name = name;
-        this.transportLine = transportLine;
     }
 
     // getters and setters for the fields
     public void setName(String name) {
         this.name = name;
     }
-    
-    public void setTransportLine(TransportLine transportLine) {
-        this.transportLine = transportLine;
-    }
 
     public String getName() {
         return name;
-    }
-    public TransportLine getTransportLine() {
-        return transportLine;
     }
 }
