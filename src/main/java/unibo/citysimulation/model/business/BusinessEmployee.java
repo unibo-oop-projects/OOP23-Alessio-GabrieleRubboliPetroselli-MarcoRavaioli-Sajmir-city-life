99aaf033505d1c8b1abb0decc2548ecce47c55d7
Alessio <alessiobifulco24@gmail.com>
2024-04-29 16:12:53 +0200

    fixed warning in BusinessEmployee

--- src/main/java/unibo/citysimulation/model/business/BusinessEmployee.java
+++ src/main/java/unibo/citysimulation/model/business/BusinessEmployee.java
@@ -1,7 +1,4 @@
 package unibo.citysimulation.model.business;
-
-import java.util.List;
-
 /**
  * Interface representing employee-related aspects of a business entity.
  * Provides methods for hiring, firing, counting, and retrieving employees.
