df2c9707cba64e95f52d544426cf7229973fb631
Sajmir <sajmir.buzi@studio.unibo.it>
2024-05-01 17:41:10 +0200

    deleted exstension

--- src/main/java/unibo/citysimulation/model/transport/TransportState.java
+++ src/main/java/unibo/citysimulation/model/transport/TransportState.java
@@ -3,7 +3,7 @@ package unibo.citysimulation.model.transport;
 /**
  * An interface for monitoring the state of transports.
  */
-public interface TransportState extends TransportLine {
+public interface TransportState{
     /**
      * Monitors the state of transports.
      */
