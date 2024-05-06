c50d045dbaea872a02adfb4a390d84ac007c912c
Sajmir <sajmir.buzi@studio.unibo.it>
2024-05-06 15:24:44 +0200

    added interface

--- src/main/java/unibo/citysimulation/model/transport/TransportLine.java
+++ src/main/java/unibo/citysimulation/model/transport/TransportLine.java
@@ -1,29 +1,29 @@
 package unibo.citysimulation.model.transport;
 
-
 /**
  * Represents a transport line within the city simulation.
+ * Each transport line has a name, capacity, and duration.
+ * It also maintains the number of people in line and can calculate the congestion.
  */
-public class TransportLine {
-    private int capacity;
-    private String name;
-    private int personInLine=0;
-    private int duration;
+public interface TransportLine {
+    /**
+     * Returns the name of the transport line.
+     *
+     * @return the name of the transport line
+     */
+    String getName();
 
-    public TransportLine(String name,int capacity,int duration) {
-        this.name = name;
-        this.capacity=capacity;
-        this.duration=duration;
-    }
+    /**
+     * Returns the congestion of the transport line as a percentage of the capacity.
+     *
+     * @return the congestion of the transport line
+     */
+    double getCongestion();
 
-    // Getter and setter methods for other fields, if needed
-    public String getName() {
-        return name;
-    }
-    public double getCongestion(){
-        return (double)(personInLine*100/capacity);
-    }
-    public int getDuration(){
-        return duration;
-    }
+    /**
+     * Returns the duration of the transport line.
+     *
+     * @return the duration of the transport line
+     */
+    int getDuration();
 }
