209ceea5deab1892281151d363403c68b5779a05
MarcoRavaioli <marco.ravaioli8@studio.unibo.it>
2024-05-13 00:16:39 +0200

    Modified the logic for screenSize in Jframe

--- src/main/java/unibo/citysimulation/controller/WindowController.java
+++ src/main/java/unibo/citysimulation/controller/WindowController.java
@@ -1,7 +1,10 @@
 package unibo.citysimulation.controller;
 
 import unibo.citysimulation.model.CityModel;
+import unibo.citysimulation.utilities.Pair;
 import unibo.citysimulation.view.WindowView;
+
+import java.awt.Dimension;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
@@ -10,6 +13,7 @@ import java.awt.event.ComponentEvent;
  */
 public class WindowController {
     private WindowView windowView;
+    private CityModel cityModel;
 
     /**
      * Constructs a WindowController with the specified models and view.
@@ -20,8 +24,12 @@ public class WindowController {
      */
     public WindowController(WindowView windowView, CityModel cityModel) {
         this.windowView = windowView;
+        this.cityModel = cityModel;
 
         this.windowView.addResizeListener(new ResizeListener());
+
+        cityModel.setFrameSize(cityModel.getScreenSize());      //questo comando è inutile se non si vogliono salvare le dimensioni nel cityModel
+
 
         new MapController(cityModel, windowView.getInfoPanel(), windowView.getMapPanel());
 
@@ -30,6 +38,9 @@ public class WindowController {
         new InputController(cityModel, windowView.getInputPanel(),windowView.getClockPanel(),windowView.getGraphicsPanel(),windowView.getMapPanel());
 
         new GraphicsController(cityModel, windowView.getGraphicsPanel());
+        
+        windowView.setFrame(cityModel.getScreenSize());
+        windowView.repaint();
     }
 
     /**
@@ -43,24 +54,25 @@ public class WindowController {
             int newWidth = e.getComponent().getBounds().width;
             int newHeight = e.getComponent().getBounds().height;
 
-            if (newWidth != windowView.getWidth() && newHeight != windowView.getHeight()) {
+            if (newWidth != cityModel.getFrameWidth() && newHeight != cityModel.getFrameHeight()) {
                 // Trova la dimensione più piccola tra nuova larghezza e altezza
                 int minSize = Math.min(newWidth, newHeight * 2);
                 // Imposta le nuove dimensioni mantenendo la proporzione 2:1
                 newWidth = minSize;
                 newHeight = minSize / 2;
             } else {
-                if (newHeight != windowView.getHeight() && newWidth == windowView.getWidth()) {
+                if (newHeight != cityModel.getFrameHeight() && newWidth == cityModel.getFrameWidth()) {
                     newWidth = (int) ((double) newHeight * 2);
                 } else {
                     newHeight = (int) ((double) newWidth / 2);
                 }
             }
 
-            windowView.setWidth(newWidth);
-            windowView.setHeight(newHeight);
-            windowView.updatePanelSize();
+            cityModel.setFrameSize(new Pair<Integer,Integer>(newWidth, newHeight));        //questa informazione è inutile, ricontrollare, al massimo la togli anche da cityModel
+            windowView.updateFrame(newWidth, newHeight);
             windowView.repaint();
         }
     }
+
+
 }
--- src/main/java/unibo/citysimulation/view/WindowView.java
+++ src/main/java/unibo/citysimulation/view/WindowView.java
@@ -19,8 +19,8 @@ public class WindowView extends JFrame {
     private int width;
     private int height;
 
-    private MapPanel mapPanel = new MapPanel();
-    private InfoPanel infoPanel = new InfoPanel(Color.GREEN);
+    private MapPanel mapPanel = new MapPanel();                                 // questi andrebbero dentro ad una lista
+    private InfoPanel infoPanel = new InfoPanel(Color.GREEN);                   // List<StyledPanel> e gestirli tutti insieme
     private ClockPanel clockPanel = new ClockPanel(Color.RED);
     private InputPanel inputPanel = new InputPanel(Color.BLUE);
     private GraphicsPanel graphicsPanel = new GraphicsPanel(Color.YELLOW);
@@ -29,12 +29,7 @@ public class WindowView extends JFrame {
      * Constructs a WindowView with the specified window model and map model.
      */
     public WindowView() {
-        Pair<Integer,Integer> size = getWindowSize();
-        this.width = size.getFirst();
-        this.height = size.getSecond();
         
-
-        setSize(width, height);
         setMinimumSize(new Dimension(ConstantAndResourceLoader.SCREEN_MINIMUM_WIDTH_PIXEL, ConstantAndResourceLoader.SCREEN_MINIMUM_HEIGHT_PIXEL));
 
         setTitle(ConstantAndResourceLoader.APPLICATION_NAME);
@@ -60,7 +55,7 @@ public class WindowView extends JFrame {
     /**
      * Updates the size of the panels based on the window size.
      */
-    public void updatePanelSize() {
+    public void updateFrame(int width, int height) {
         setSize(new Dimension(width, height));
 
         inputPanel.setPreferredSize(new Dimension(width / 4, height));
@@ -109,13 +104,13 @@ public class WindowView extends JFrame {
 
         // Add input panel and info panel to left panel
         constraints.gridy = 0;
-        constraints.weighty = 0.75;
-        inputPanel.setPreferredSize(new Dimension(sidePanelWidth, sidePanelsHeight / 4 * 3));
+        constraints.weighty = 0.625;
+        inputPanel.setPreferredSize(new Dimension(sidePanelWidth, sidePanelsHeight / 8 * 5));
         leftPanel.add(inputPanel, constraints);
 
         constraints.gridy = 1;
-        constraints.weighty = 0.25;
-        infoPanel.setPreferredSize(new Dimension(sidePanelWidth, sidePanelsHeight / 4));
+        constraints.weighty = 0.375;
+        infoPanel.setPreferredSize(new Dimension(sidePanelWidth, sidePanelsHeight / 8 * 3));
         leftPanel.add(infoPanel, constraints);
 
         // Add clock panel and graphics panel to right panel
@@ -134,62 +129,14 @@ public class WindowView extends JFrame {
         add(rightPanel, BorderLayout.EAST);
     }
 
-    /**
-     * Retrieves the size of the window based on screen dimensions.
-     *
-     * @return The pair of width and height.
-     */
-    private Pair<Integer,Integer> getWindowSize() {
-        // Get the screen size
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public void setFrame(Pair<Integer, Integer> frameSize) {
+        this.width = frameSize.getFirst();
+        this.height = frameSize.getSecond();
+        //setSize(width, height);
 
-        // Calculate the maximum dimensions based on the screen size and a constant percentage
-        int maxWidth = (int) (screenSize.getWidth() * ConstantAndResourceLoader.SCREEN_SIZE_PERCENTAGE);
-        int maxHeight = (int) (screenSize.getHeight() * ConstantAndResourceLoader.SCREEN_SIZE_PERCENTAGE);
-
-        // Calculate the frame dimensions based on the maximum dimensions
-        int frameHeight = maxHeight > maxWidth / 2 ? maxWidth / 2 : maxHeight;
-        int frameWidth = frameHeight * 2;
-
-        // Create and return the window model with the calculated dimensions
-        return new Pair<>(frameWidth, frameHeight);
+        updateFrame(width, height);
     }
 
-    /**
-     * Gets the width of the window.
-     *
-     * @return The width of the window.
-     */
-    public int getWidth() {
-        return width;
-    }
-
-    /**
-     * Gets the height of the window.
-     *
-     * @return The height of the window.
-     */
-    public int getHeight() {
-        return height;
-    }
-
-    /**
-     * Sets the width of the window.
-     *
-     * @param width The width of the window.
-     */
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    /**
-     * Sets the height of the window.
-     *
-     * @param height The height of the window.
-     */
-    public void setHeight(int height) {
-        this.height = height;
-    }
 
     /**
      * Retrieves the info panel.
