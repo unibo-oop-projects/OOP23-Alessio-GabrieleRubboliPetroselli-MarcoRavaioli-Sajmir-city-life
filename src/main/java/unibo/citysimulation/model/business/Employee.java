f53e6694f4b21eec2cb9028f41673af0e8dea9c4
Alessio <alessiobifulco24@gmail.com>
2024-05-15 12:24:10 +0200

    Person now is a field of employee, employee does not extend person anymore

--- src/main/java/unibo/citysimulation/model/business/Employee.java
+++ src/main/java/unibo/citysimulation/model/business/Employee.java
@@ -1,20 +1,32 @@
 package unibo.citysimulation.model.business;
 
+import unibo.citysimulation.model.person.Person;
+
+
+
 /**
- * This interface represents an employee with a name and an ID.
+ * EmployeeImpl is a class that implements the Employee interface.
+ * It represents an employee with a name and an ID.
  */
-public interface Employee {
- 
-    /**
-     * Returns the name of the employee.
-     *
-     * @return The name of the employee.
-     */
-    String getName();
-
-    int getExperience();
-
-    int getcountDelay();
-
+public class Employee implements EmployeeStatus{
     
+    private int countDelay;
+    private Person person;
+
+    public Employee(Person person){
+        this.person = person;
+        this.countDelay = 0;
+
+    }
+
+    @Override
+    public int getcountDelay(Employee employee) {
+        return employee.countDelay;
+    }
+
+    @Override
+    public void setcountDelay(int countDelay) {
+        this.countDelay = countDelay;
+    }
+
 }
---
+++ src/main/java/unibo/citysimulation/model/business/EmployeeStatus.java
@@ -0,0 +1,22 @@
+package unibo.citysimulation.model.business;
+
+
+/**
+ * This interface represents an employee with a name and an ID.
+ */
+public interface EmployeeStatus {
+
+
+    /**
+     * Returns the name of the employee.
+     *
+     * @return The name of the employee.
+     */
+    
+
+    int getcountDelay(Employee employee);
+
+    void setcountDelay(int countDelay);
+
+    
+}
