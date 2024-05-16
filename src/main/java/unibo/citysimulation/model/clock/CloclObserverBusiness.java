97657477b956373205d8b0a313704c840cb2ba03
Alessio <alessiobifulco24@gmail.com>
2024-05-16 10:14:45 +0200

    added hire logic

--- src/main/java/unibo/citysimulation/model/clock/CloclObserverBusiness.java
+++ src/main/java/unibo/citysimulation/model/clock/CloclObserverBusiness.java
@@ -8,7 +8,10 @@ import java.time.LocalTime;
 
 public class CloclObserverBusiness implements ClockObserver{
     
+    private static final int DEFAULT_DAY = 0;
+    private static final int WEEK = 7;
     private List<Business> businesses;
+    private int lastKnownDay = DEFAULT_DAY;
     
     public CloclObserverBusiness(List<Business> businesses) {
         this.businesses = businesses;
@@ -16,11 +19,22 @@ public class CloclObserverBusiness implements ClockObserver{
     
     @Override
     public void onTimeUpdate(LocalTime currentTime, int currentDay) {
+        if(currentDay != lastKnownDay){
         for (Business business : businesses) {
             for (Employee employee : business.getEmployees()) {
                 business.fire(employee, business);
             }
         }
-        
+        }
+    lastKnownDay = currentDay;
+        if (currentDay % WEEK == 0) {
+        for (Business business : businesses) {
+            if(business.getEmployees().size() < business.getMaxEmployees()){
+            for (Employee employee : business.getEmployees()) {
+                business.hire(employee, business);
+            }
+        }
+        }
+        }
     }
 }
