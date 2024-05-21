f554cb8d80ccda78a7a559fde2e8dcd007aad0c1
Sajmir <sajmir.buzi@studio.unibo.it>
2024-05-21 15:13:21 +0200

    added recalling of the legendPanel into graphicPanel

--- src/main/java/unibo/citysimulation/view/sidePanels/GraphicsPanel.java
+++ src/main/java/unibo/citysimulation/view/sidePanels/GraphicsPanel.java
@@ -24,8 +24,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 /**
  * Panel for displaying graphics.
@@ -41,6 +39,7 @@ public class GraphicsPanel extends StyledPanel {
     private double maxCongestionHeight = 1;
     private List<TransportLine> transportLines;
     private List<Zone> zones = ZoneFactory.createZonesFromFile();
+    private LegendPanel legendButton;
 
     /**
      * Constructs a GraphicsPanel with the specified background color.
@@ -49,90 +48,34 @@ public class GraphicsPanel extends StyledPanel {
      */
     public GraphicsPanel(Color bgColor) {
         super(bgColor);
-        this.transportLines=TransportFactory.createTransportsFromFile(zones);
-        JButton legendButton = new JButton("Legend");
-        legendButton.setPreferredSize(new Dimension(20, 20)); // Set the preferred size of the button to make it small
-        legendButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame legendFrame = new JFrame("Graph Legend");
-                legendFrame.setSize(300, 300);
-                legendFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-                JPanel legendPanel = new JPanel();
-                legendPanel.setLayout(new BoxLayout(legendPanel, BoxLayout.Y_AXIS));
+        this.transportLines = TransportFactory.createTransportsFromFile(zones);
 
-                JLabel title = new JLabel("Graph Legend");
-                title.setFont(new Font("Serif", Font.BOLD, 18));
-                legendPanel.add(title);
-                legendPanel.add(Box.createVerticalStrut(10)); // Spacing
-                JLabel divisionTitle = new JLabel("People State:");
-                divisionTitle.setFont(new Font("Serif", Font.BOLD, 18));
-                legendPanel.add(divisionTitle);
+        this.legendButton = new LegendPanel(colors, transportLines);
+        this.legendButton.setPreferredSize(new Dimension(200, 50)); // Set the preferred size of the button to make it small
 
-                legendPanel.add(createLegendItem("AT_WORKING", Color.RED));
-                legendPanel.add(createLegendItem("AT_HOME", Color.BLUE));
-                legendPanel.add(createLegendItem("MOVING", Color.YELLOW));
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); // Modifica layout per meglio posizionare il bottone
+        topPanel.add(legendButton);
 
-                legendPanel.add(Box.createVerticalStrut(10)); // Spacing
-                JLabel transportTitle = new JLabel("Transport Congestion:");
-                transportTitle.setFont(new Font("Serif", Font.BOLD, 18));
-                legendPanel.add(transportTitle);
-
-                // Add transport lines to the legend
-                for (int i = 0; i < transportLines.size(); i += 3) {
-                    JPanel groupPanel = new JPanel();
-                    groupPanel.setLayout(new BoxLayout(groupPanel, BoxLayout.Y_AXIS));
-                    for (int j = 0; j < 3 && (i + j) < transportLines.size(); j++) {
-                        TransportLine line = transportLines.get(i + j);
-                        Color color = colors.get((i + j) % colors.size());
-                        groupPanel.add(createLegendItem(line.getName(), color));
-                    }
-                    legendPanel.add(groupPanel);
-                }
-
-                JScrollPane scrollPane = new JScrollPane(legendPanel);
-                legendFrame.add(scrollPane);
-                legendFrame.setVisible(true);
-            }
-        });
-        JPanel topPanel = new JPanel(new BorderLayout());
-        topPanel.add(legendButton, BorderLayout.NORTH);
-    
         // Add the top panel to this panel
+        this.setLayout(new BorderLayout()); // Imposta layout di GraphicsPanel
         this.add(topPanel, BorderLayout.NORTH);
-    
+
         names = Arrays.asList("Person State", "Transport Congestion", "Business");
-    
+
         datasets = createDatasets(names.size());
         List<JFreeChart> charts = createCharts(names, datasets);
-    
+
         plots = charts.stream()
                 .map(JFreeChart::getXYPlot)
                 .peek(plot -> plot.setRenderer(createRenderer(plot.getSeriesCount())))
                 .collect(Collectors.toList());
-    
-        synchronized (this) {
-            plots.forEach(plot -> add(new ChartPanel(plot.getChart())));
-        }
-    
-        plots.get(0).getRangeAxis().setTickLabelsVisible(false);
-        plots.get(1).getRangeAxis().setTickLabelsVisible(false);
-    
-        // Set layout to arrange charts vertically
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-    }
 
-    private JPanel createLegendItem(String text, Color color) {
-        JPanel itemPanel = new JPanel();
-        itemPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        JLabel colorLabel = new JLabel();
-        colorLabel.setOpaque(true);
-        colorLabel.setBackground(color);
-        colorLabel.setPreferredSize(new Dimension(10, 10));
-        itemPanel.add(colorLabel);
-        itemPanel.add(new JLabel(text));
-        return itemPanel;
+        JPanel chartPanel = new JPanel();
+        chartPanel.setLayout(new BoxLayout(chartPanel, BoxLayout.Y_AXIS));
+        plots.forEach(plot -> chartPanel.add(new ChartPanel(plot.getChart())));
+
+        this.add(chartPanel, BorderLayout.CENTER);
     }
 
     private List<XYSeriesCollection> createDatasets(int num) {
