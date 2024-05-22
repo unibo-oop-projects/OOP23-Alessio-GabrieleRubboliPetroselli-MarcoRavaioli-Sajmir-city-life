ebfc9fa0ad72f01a2b89d98f8311de1bcfd4b136
Alessio <alessiobifulco24@gmail.com>
2024-05-22 13:18:37 +0200

    now hiring happen when a business open, and fire when it close

--- src/main/java/unibo/citysimulation/model/clock/CloclObserverBusiness.java
+++ src/main/java/unibo/citysimulation/model/clock/CloclObserverBusiness.java
@@ -12,12 +12,8 @@ import java.time.LocalTime;
  */
 public class CloclObserverBusiness implements ClockObserver{
     
-    private static final int DEFAULT_DAY = 0;
-    private static final int WEEK = 7;
     private final List<Business> businesses;
-    private int lastKnownDay = DEFAULT_DAY;
-
-    private EmployymentOfficeManager employmentManager;
+    private final EmployymentOfficeManager employmentManager;
     
     /**
      * Constructs a CloclObserverBusiness object with the given list of businesses and employment office.
@@ -29,8 +25,7 @@ public class CloclObserverBusiness implements ClockObserver{
         this.businesses = businesses;
         this.employmentManager = new EmployymentOfficeManager(businesses, employymentOffice);
 
-        // Assumi dipendenti iniziali
-        this.employmentManager.handleEmployeeHiring();
+        
     }
     
     /**
@@ -40,17 +35,20 @@ public class CloclObserverBusiness implements ClockObserver{
      * @param currentDay the current day
      */
     @Override
-    public void onTimeUpdate(final LocalTime currentTime,final int currentDay) {
-        if (currentDay != lastKnownDay) {
-            employmentManager.handleEmployeeFiring();
-            lastKnownDay = currentDay;
+    public void onTimeUpdate(final LocalTime currentTime, final int currentDay) {
+        for (Business business : businesses) {
+            if (currentTime.equals(business.getOpLocalTime())) {
+                employmentManager.handleEmployeeHiring(business);
+            }
+            if (currentTime.equals(business.getClLocalTime())) {
+                employmentManager.handleEmployeeFiring(business);
+            }
         }
-        if (currentDay % WEEK == 0) {
-            employmentManager.handleEmployeeHiring();
-        }
+
         for (Business business : businesses) {
             business.checkEmployeeDelays(currentTime);
         }
     }
 }
+
 
