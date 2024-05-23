502597537250b6ff402386e16c465b32394df9d7
MarcoRavaioli <marco.ravaioli8@studio.unibo.it>
2024-05-23 17:15:13 +0200

    Corrected merge errors

--- src/main/java/unibo/citysimulation/WelcomeScreen.java
+++ src/main/java/unibo/citysimulation/WelcomeScreen.java
@@ -2,6 +2,7 @@ package unibo.citysimulation;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import javax.swing.JPanel;
