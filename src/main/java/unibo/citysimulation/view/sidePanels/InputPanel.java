2a2200aa769fd8d75d25f192ad6e0a08053c00fe
Sajmir <sajmir.buzi@studio.unibo.it>
2024-05-29 13:44:34 +0200

    resolved spotBugs errors on inputPanel and Controller

--- src/main/java/unibo/citysimulation/view/sidePanels/InputPanel.java
+++ src/main/java/unibo/citysimulation/view/sidePanels/InputPanel.java
@@ -16,6 +16,7 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
 /**
  * This class represents the input panel.
  */
@@ -84,21 +85,21 @@ public class InputPanel extends StyledPanel {
         gbc.gridwidth = 2; // Occupies two columns
         add(buttonPanel, gbc);
     }
-     /**
-     * Returns the start button.
+    /**
+     * Adds an action listener to the start button.
      *
-     * @return the start button
+     * @param listener the action listener to be added to the start button
      */
-    public JButton getStartButton() {
-        return startButton;
+    public void addStartButtonListener(ActionListener listener) {
+        startButton.addActionListener(listener);
     }
     /**
-     * Returns the stop button.
+     * Adds an action listener to the stop button.
      *
-     * @return the stop button
+     * @param listener the action listener to be added to the stop button
      */
-    public JButton getStopButton() {
-        return stopButton;
+    public void addStopButtonListener(ActionListener listener) {
+        stopButton.addActionListener(listener);
     }
     /**
      * Returns the value of the people slider.
@@ -117,28 +118,35 @@ public class InputPanel extends StyledPanel {
         return capacitySlider.getValue();
     }
     /**
-     * Returns the people slider.
-     *
-     * @return the people slider
-     */
-    public JSlider getPeopleSlider() {
-        return peopleSlider;
-    }
-    /**
-     * Returns the capacity slider.
-     *
-     * @return the capacity slider
-     */
-    public JSlider getCapacitySlider() {
-        return capacitySlider;
-    }
-    /**
      * Returns the value of the business slider.
      *
      * @return the value of the business slider
      */
-    public JSlider getBusinessSlider() {
-        return businessSlider;
+    public int getBusinessSliderValue() {
+        return businessSlider.getValue();
+    }
+    /**
+     * set the start button enabled or disabled
+     * @param enabled
+     */
+    public void setStartButtonEnabled(boolean enabled) {
+        startButton.setEnabled(enabled);
+    }
+    /**
+     * set the stop button enabled or disabled
+     * @param enabled
+     */
+    public void setStopButtonEnabled(boolean enabled) {
+        stopButton.setEnabled(enabled);
+    }
+    /**
+     * set the sliders enabled or disabled
+     * @param enabled
+     */
+    public void setSlidersEnabled(boolean enabled) {
+        peopleSlider.setEnabled(enabled);
+        capacitySlider.setEnabled(enabled);
+        businessSlider.setEnabled(enabled);
     }
     private JSlider createSlider(final String title, final int min, final int max) {
         final JSlider slider = new JSlider(min, max);
