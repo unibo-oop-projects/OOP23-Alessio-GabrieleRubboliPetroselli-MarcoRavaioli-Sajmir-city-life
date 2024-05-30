c2aef617484e6038fd90474121a4f30fc544e76b
Alessio <alessiobifulco24@gmail.com>
2024-05-30 21:41:16 +0200

    used record

--- src/main/java/unibo/citysimulation/model/business/employye/api/EmploymentOfficeData.java
+++ src/main/java/unibo/citysimulation/model/business/employye/api/EmploymentOfficeData.java
@@ -1,9 +1,8 @@
-package unibo.citysimulation.model.business;
+package unibo.citysimulation.model.business.employye.api;
 
 import java.util.List;
 
-import unibo.citysimulation.model.person.DynamicPerson;
-
+import unibo.citysimulation.model.person.api.DynamicPerson;
 /**
  * The EmploymentOffice class represents an employment office in a city simulation.
  * It keeps track of disoccupied people and provides methods to add and remove them.
