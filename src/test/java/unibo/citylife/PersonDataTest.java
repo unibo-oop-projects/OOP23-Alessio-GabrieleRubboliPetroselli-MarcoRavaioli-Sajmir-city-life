b13e5d0d7471ce7ef6ca65501b56fcc84b1307ad
Gabriele Rubboli Petroselli <gabriele.rubboli@studio.unibo.it>
2024-06-04 16:46:02 +0200

    person test fixed

--- src/test/java/unibo/citylife/PersonDataTest.java
+++ src/test/java/unibo/citylife/PersonDataTest.java
@@ -1,15 +1,16 @@
 package unibo.citylife;
-/* 
+
 import org.junit.jupiter.api.Test;
 
 import unibo.citysimulation.model.business.impl.Business;
 import unibo.citysimulation.model.business.impl.BusinessFactory;
-import unibo.citysimulation.model.person.PersonData;
+import unibo.citysimulation.model.person.api.PersonData;
 import unibo.citysimulation.model.zone.Zone;
 import unibo.citysimulation.model.zone.ZoneFactory;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,13 +24,12 @@ class PersonDataTest {
         final int age = 30;
         final Zone residenceZone = zones.get(random.nextInt(zones.size()));
         final Business business = BusinessFactory.getRandomBusiness(zones).get();
-        final PersonData personData = new PersonData(name, age, business, residenceZone);
+        final PersonData personData = new PersonData(name, age, Optional.of(business), residenceZone);
 
         assertNotNull(personData);
         assertEquals(name, personData.name());
         assertEquals(age, personData.age());
-        assertEquals(business, personData.business());
+        assertEquals(business, personData.business().get());
         assertEquals(residenceZone, personData.residenceZone());
     }
 }
-*/
