b9a0cec37cce84e440499588a6847d085af59f2b
Sajmir <sajmir.buzi@studio.unibo.it>
2024-06-06 20:46:45 +0200

    added interface InfoModel

---
+++ src/main/java/unibo/citysimulation/model/InfoModel.java
@@ -0,0 +1,15 @@
+package unibo.citysimulation.model;
+
+/**
+ * This interface represents the model for the information panel.
+ */
+public interface InfoModel {
+    /**
+     * Updates the information panel based on the coordinates provided.
+     *
+     * @param x The x-coordinate.
+     * @param y The y-coordinate.
+     */
+    void updateZoneInfo(final int x, final int y); 
+}
+
